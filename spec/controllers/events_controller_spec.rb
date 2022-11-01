49812ee09a32421955f31bcc95ff5e49a66e3320
Mandydidi <90797792+Mandydidi@users.noreply.github.com>
2022-11-01 16:28:55 -0400

    added tdd cases for myEvents and index (#44)
    * added test cases for myEvents
    
    * added tdd fulfilling index

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -13,6 +13,14 @@ describe EventsController do
             it "should render index template" do                     
                 expect(response).to render_template("index")
             end
+        end 
+
+        context "Search 'David' in the search bar" do
+            it "should render search" do
+                get :index, :q => 'David'
+                expect(Event).to receive(:find_event_by_name).with('David').and_return(event)
+                Event.find_event_by_name('David')
+            end
         end
     end
 
@@ -37,4 +45,22 @@ describe EventsController do
             end
         end
     end
+
+    describe "#myEvents" do
+        let!(:event) {FactoryGirl.create(:event)}
+        let!(:user) {FactoryGirl.build(:user)}
+        before do
+            sign_in user
+            get :myEvents
+        end
+
+        context "When go to the myEvents page" do
+            it "should render myEvents template and show hosted/joined events" do
+                expect(response).to render_template("myEvents")
+                Event.find_all_host_events(user.username)
+                Event.find_all_join_events(user.username)
+            end 
+        end
+
+    end 
 end
