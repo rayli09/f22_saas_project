49a58c5b928fa72465b5c8f91235fa52c02b9ac9
JenniferDuan <jd3794@columbia.edu>
2022-11-01 17:28:48 -0400

    [t1] tdd-RSpec-test-event. (#45)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221029213716) do
+ActiveRecord::Schema.define(version: 20221030194138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,8 +39,16 @@ ActiveRecord::Schema.define(version: 20221029213716) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,6 +1,132 @@
 require 'rails_helper'
 
 describe EventsController do
+    describe '#show' do
+        let!(:event) {FactoryGirl.create(:event)}
+        let!(:user) {FactoryGirl.build(:user)}
+        before do
+            sign_in user
+            get :show, {:id => event.id}
+        end
+
+        context 'When go to the event detail page' do
+            it 'should have the join button' do
+                expect(assigns(:join_text)).to eq(:Join)
+            end
+
+            it 'should have join button style' do
+                expect(assigns(:join_btn_style)).to eq('btn btn-success col-2')
+            end
+
+            it 'should not be host' do
+                expect(assigns(:is_viewer_host)).to eq(false)
+            end
+
+            it 'should render show template' do
+                expect(response).to render_template('show')
+            end
+        end
+    end
+
+    describe '#new' do
+        let!(:user) {FactoryGirl.build(:user)}
+        before do
+            sign_in user
+            get :new
+        end
+
+        context 'When go to the post event page' do
+            it 'should render new event template' do
+                expect(response).to render_template('new')
+            end
+        end
+    end
+
+    describe '#create' do
+        let!(:user) {FactoryGirl.build(:user)}
+        before do
+            sign_in user
+        end
+
+        context 'When successfully post a new event' do
+            it 'should store the event in the database and redirect to home page' do
+                params = ActionController::Parameters.new(:title => 'Test lunch', :description => 'Eat together', :event_time => '05-Nov-2022', :attendee_limit => 4)
+                expect{post :create, {:event => params}}.to change{Event.count}.by(1)
+                expect(response).to redirect_to(events_path)
+            end
+        end
+
+        context 'When failed to post a new event due to missing title' do
+            it 'should not store the event in the database and should redirect to new event page' do
+                params = ActionController::Parameters.new(:description => 'Eat together', :event_time => '05-Nov-2022', :attendee_limit => 4)
+                expect{post :create, {:event => params}}.to change{Event.count}.by(0)
+                expect(response).to redirect_to(new_event_path)
+            end
+        end
+    end
+
+    describe '#edit' do
+        let!(:event) {FactoryGirl.create(:event)}
+        let!(:user) {FactoryGirl.build(:user)}
+        before do
+            sign_in user
+            get :edit, {:id => event.id}
+        end
+
+        context 'When go to the edit event page' do
+            it 'should render edit event template' do
+                expect(assigns(:event)).to eq(event)
+                expect(response).to render_template('edit')
+            end
+        end
+    end
+
+    describe '#update' do
+        let!(:event) {FactoryGirl.create(:event)}
+        let!(:user) {FactoryGirl.build(:user)}
+        before do
+            sign_in user
+        end
+
+        context 'When successfully update an existing event' do
+            it 'should store the update in the database and redirect to event details page' do
+                params = ActionController::Parameters.new(:title => 'Test lunch', :description => 'Change description', :attendee_limit => 10)
+                put :update, {:id => event.id, :event => params}
+                event.reload
+                expect(event.title).to eq('Test lunch')
+                expect(event.description).to eq('Change description')
+                expect(event.attendee_limit).to eq(10)
+                expect(response).to redirect_to(event_path(event))
+            end
+        end
+
+        context 'When failed to update an existing event due to missing attendee limit' do
+            it 'should not change event info in the database and should redirect to edit event page' do
+                params = ActionController::Parameters.new(:title => 'Test lunch', :description => 'Change description')
+                put :update, {:id => event.id, :event => params}
+                expect(event.title).to eq('Lunch')
+                expect(event.description).to eq(nil)
+                expect(response).to redirect_to(edit_event_path(event))
+            end
+        end
+    end
+
+    describe '#destroy' do
+        let!(:event) {FactoryGirl.create(:event)}
+        let!(:user) {FactoryGirl.build(:user)}
+        before do
+            sign_in user
+        end
+
+        context 'When successfully delete an existing event' do
+            it 'should delete the event from the database and redirect to home page' do
+                expect{delete :destroy, {:id => event.id}}.to change{Event.count}.by(-1)
+                expect(response).to redirect_to(events_path)
+            end
+
+        end
+    end
+
     describe "#index" do
         let!(:event) {FactoryGirl.create(:event)}
         let!(:user) {FactoryGirl.build(:user)}
