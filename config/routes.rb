4393bf5c3d07ba83c2038d74d3a54cbae1f52cfc
Mandydidi <734680081@qq.com>
2022-11-06 08:39:55 -0500

    del one empty line and add comment

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rottenpotatoes::Application.routes.draw do
   # map '/' to be a redirect to '/events'
   root :to => redirect('/welcome')
   
-  
   match '/myEvents', to: 'events#myEvents', as: 'myEvents', via: :get 
   match '/search', to: 'events#search', as: 'search', via: :get   #TODO? why is this GET?
   match '/search_result', to: 'events#index', as: 'search_result', via: :get
