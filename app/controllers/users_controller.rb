ac5e1614901297dbb07d1a47486abaa43f4564e5
Ruize Li <99834841+rl3250@users.noreply.github.com>
2022-11-11 10:45:22 -0500

    [t2] fixed shit (#58)
    * fixed shit
    
    * updated GH actions to run on PR
    
    * nit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(params.require(:user).permit(:username,        
-    :password))
+    if User.find_by(username: user_params["username"])
+      flash[:warning] = "username already exists"
+      redirect_to '/users/new?' and return
+    end
+    @user = User.create(user_params)
     session[:user_id] = @user.id
     redirect_to '/welcome'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:password)
   end
 end
