021a22eccd5bfe046da976b5cb8a0ce8b29e4fad
Jianyang Duan <jd3794@columbia.edu>
2022-11-14 18:02:04 -0500

    User profile feature is completed. current_user id empty error not solved and caused problems for MyProfile and Cucumber test.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,33 @@ class UsersController < ApplicationController
     redirect_to '/welcome'
   end
 
+  def show
+    @user = User.find(params[:id])
+    @is_viewer_user = @user.username == current_user.username
+    @host_events = Event.find_all_host_events(@user.username)
+    @host_events = [] if @host_events.nil? or @host_events.empty?
+    @join_events = Event.find_all_join_events(@user.username)
+    @join_events = [] if @join_events.nil? or @join_events.empty?
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if (!user_params["email"].blank?) and (user_params["email"] !~ URI::MailTo::EMAIL_REGEXP)
+      flash[:warning] = "Email must be correctly filled in."
+      redirect_to edit_user_path(@user)
+    else
+      @user.update_attributes!(user_params)
+      flash[:notice] = "User '#{@user.username}' was successfully updated."
+      redirect_to user_path(@user)
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:username,:password)
+    params.require(:user).permit(:username, :password, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rottenpotatoes::Application.routes.draw do
-  resources :users, only: [:new, :create]
-   get 'login', to: 'sessions#new', as: 'sign_up'
-   post 'login', to: 'sessions#create', as: 'log_in'
-   get 'welcome', to: 'sessions#welcome', as: "login_signup"
-   get 'authorized', to: 'sessions#page_requires_login'
-   get 'logout', to: 'sessions#logout', as: 'logout'
+  resources :users do
+  end
+
+  get 'login', to: 'sessions#new', as: 'sign_up'
+  post 'login', to: 'sessions#create', as: 'log_in'
+  get 'welcome', to: 'sessions#welcome', as: "login_signup"
+  get 'authorized', to: 'sessions#page_requires_login'
+  get 'logout', to: 'sessions#logout', as: 'logout'
 
   resources :events do
     resources :comments, except: ['show']
