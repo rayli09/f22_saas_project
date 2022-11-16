fa43f2688eb7f7ddc7b3be607682ecdf3fa7a485
Ruize Li <rl3250@columbia.edu>
2022-11-16 10:03:01 -0500

    fixing config for sso

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
+    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], {access_type: 'online'}
 end
 OmniAuth.config.logger = Rails.logger if Rails.env.development?
