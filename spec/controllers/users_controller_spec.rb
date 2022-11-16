b5aa48600c5b299006c602f91288bf36320d9023
JenniferDuan <jd3794@columbia.edu>
2022-11-15 20:27:41 -0500

    [t2] User Profile TDD RSpec Test (#86)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,10 +11,105 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "POST #create" do
-    let!(:user) {FactoryGirl.build(:user)}
-    it "signs up a new user and go to welcome" do
-      post :create, {:user=>{:username=>user.username,:password=>user.password}}
-      expect(response).to redirect_to '/welcome'
+    let!(:user) {FactoryGirl.create(:user)}
+
+    context "When successfully create a new user" do
+      it "should store the new user in the database and go to welcome" do
+        params = ActionController::Parameters.new(:username => "smartuser", :password => "hahaha")
+        expect{post :create, {:user => params}}.to change{User.count}.by(1)
+        expect(response).to redirect_to '/welcome'
+      end
+    end
+
+    context "When fails to create a new user due to duplicate username" do
+      it "should not store the new user in the database and go to new user page" do
+        params = ActionController::Parameters.new(:username => "testuser", :password => "test")
+        expect{post :create, {:user => params}}.to change{User.count}.by(0)
+        expect(response).to redirect_to '/users/new?'
+      end
+    end
+
+    context "When fails to create a new user due to empty username" do
+      it "should not store the new user in the database and go to new user page" do
+        params = ActionController::Parameters.new(:password => "test")
+        expect{post :create, {:user => params}}.to change{User.count}.by(0)
+        expect(response).to redirect_to '/users/new?'
+      end
+    end
+  end
+
+  describe "GET #show"  do
+    let!(:user) {FactoryGirl.create(:user)}
+    before do
+      sign_in user
+      existing_user = User.create({:username => "smartuser", :password =>"smart123", :email => "smartuser@gmail.com"})
+      get :show, {:id => existing_user.id}
+    end
+
+    context 'When go to the user profile page' do
+      it 'should not be host' do
+        expect(assigns(:is_viewer_user)).to eq(false)
+      end
+
+      it 'should render show template' do
+        expect(response).to render_template('show')
+      end
+    end
+  end
+
+  describe "GET #myProfile" do
+    let!(:user) {FactoryGirl.create(:user)}
+    before do
+      sign_in user
+      get :myProfile
+    end
+
+    context "When go to the myProfile page" do
+      it "should redirect to current user's profile page" do
+        expect(response).to redirect_to(user_path(user))
+      end
+    end
+  end
+
+  describe "GET #edit" do
+    let!(:user) {FactoryGirl.create(:user)}
+    before do
+      sign_in user
+      get :edit, {:id => user.id}
+    end
+
+    context "When go to the edit user profile page" do
+      it "should render edit user template" do
+        expect(assigns(:user)).to eq(user)
+        expect(response).to render_template('edit')
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    let!(:user) {FactoryGirl.create(:user)}
+    before do
+      sign_in user
+    end
+
+    context "When successfully update an existing user profile" do
+      it "should store the update in the database and redirect to user profile page" do
+        params = ActionController::Parameters.new(:email => "testuserlol@gmail.com")
+        put :update, {:id => user.id, :user => params}
+        user.reload
+        expect(user.email).to eq("testuserlol@gmail.com")
+        expect(response).to redirect_to(user_path(user))
+      end
+    end
+
+    context "When failed to update an existing user profile due to invalid email format" do
+      it "should not change user info in the database and redirect to edit user profile page" do
+        params = ActionController::Parameters.new(:email => "testuserlol")
+        put :update, {:id => user.id, :user => params}
+        user.reload
+        expect(user.email).to eq("")
+        expect(response).to redirect_to(edit_user_path(user))
+      end
     end
   end
 
