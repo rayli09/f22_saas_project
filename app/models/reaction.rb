2633eb0f87068bfcb51562b6a986d4204d6e1ab2
Mandydidi <734680081@qq.com>
2022-11-21 17:18:29 -0500

    chan simplified count functions

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -2,13 +2,7 @@ class Reaction < ActiveRecord::Base
     belongs_to :comment, :class_name => "Comment", :foreign_key => "comment_id"
     belongs_to :user, :class_name => "User", :foreign_key => "user_id"
 
-    def self.count_likes(comment_id)
-        Reaction.where({:comment_id => comment_id, :action => 0}).count
-    end
-    def self.count_ups(comment_id)
-        Reaction.where({:comment_id => comment_id, :action => 1}).count
-    end
-    def self.count_downs(comment_id)
-        Reaction.where({:comment_id => comment_id, :action => 2}).count
+    def self.count_reacts(comment_id, action_id)
+        Reaction.where({:comment_id => comment_id, :action => action_id}).count
     end
 end
