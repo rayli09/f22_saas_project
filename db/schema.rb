0e9d3ef94974d88adf0454e34b80c5316a45347f
Ruize Li <rl3250@columbia.edu>
2022-11-29 09:45:21 -0500

    update schema to include coins

--- db/schema.rb
+++ db/schema.rb
@@ -44,12 +44,13 @@ ActiveRecord::Schema.define(version: 20221113213413) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.integer  "rating",                 default: 5,  null: false
-    t.integer  "num_rating_got",         default: 1,  null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.integer  "rating",                 default: 5,   null: false
+    t.integer  "num_rating_got",         default: 1,   null: false
+    t.integer  "coins",                  default: 100, null: false
+    t.string   "email",                  default: "",  null: false
+    t.string   "encrypted_password",     default: "",  null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
